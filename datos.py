7c2618ec8bb716ac7191debcfcab8aeb29dcd00c
estavillo97 <31199484+estavillo97@users.noreply.github.com>
2020-03-06 12:58:51 -0600

    visualizaciones
    visualizaciones

---
+++ datos.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  6 12:53:40 2020
+
+@author: Estavillo
+"""
+
+#aqui se agregará todo lo necesario para poder trabajar en el lab 2 de myst
---
+++ visualizaciones .py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  6 12:56:10 2020
+
+@author: Estavillo
+"""
+
+#aqui iran las visualizaciones  
+a=0
