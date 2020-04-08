c9704a6bee9068b32cf28b1e574abb8ac44e35c6
estavillo97 <31199484+estavillo97@users.noreply.github.com>
2020-04-07 22:59:11 -0500

    avance de funciones
    aumento de funciones

--- funciones.py
+++ funciones.py
@@ -1,21 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 19 12:27:53 2020
 
+@author: Estavillo
+"""
 # -- ------------------------------------------------------------------------------------ -- #
-# -- Proyecto: Describir brevemente el proyecto en general                                -- #
-# -- Codigo: RepasoPython.py - describir brevemente el codigo                             -- #
-# -- Repositorio: https://github.com/                                                     -- #
-# -- Autor: Nombre de autor                                                               -- #
+# -- Proyecto: laboratorio 2 de trading.                               -- #
+# -- Codigo: lab2 - elaborar herramientas de análisis                           -- #
+# -- Repositorio: https://github.com/estavillo97/LAB_2_JPEU/funciones.py      --#                                                    -- #
+# -- Autor: Juan Pablo Estavillo Urrea                                                                -- #
 # -- ------------------------------------------------------------------------------------ -- #
-
-# import numpy as np                                      # funciones numericas
+from oandapyV20 import API                                 # conexion con broker OANDA
+import numpy as np                                      # funciones numericas
 import pandas as pd                                       # dataframes y utilidades
-from datetime import timedelta                            # diferencia entre datos tipo tiempo
-from oandapyV20 import API                                # conexion con broker OANDA
+from datetime import timedelta                            # diferencia entre datos tipo tiempo          
 import oandapyV20.endpoints.instruments as instruments    # informacion de precios historicos
-
 
 # -- --------------------------------------------------------- FUNCION: Descargar precios -- #
 # -- Descargar precios historicos con OANDA
-
+# añadi estas librerias de Francisco por si se llegaran a ocupar
 def f_precios_masivos(p0_fini, p1_ffin, p2_gran, p3_inst, p4_oatk, p5_ginc):
     """
     Parameters
@@ -26,30 +29,24 @@ def f_precios_masivos(p0_fini, p1_ffin, p2_gran, p3_inst, p4_oatk, p5_ginc):
     p3_inst
     p4_oatk
     p5_ginc
-
     Returns
     -------
     dc_precios
-
     Debugging
     ---------
-
     """
 
     def f_datetime_range_fx(p0_start, p1_end, p2_inc, p3_delta):
         """
-
         Parameters
         ----------
         p0_start
         p1_end
         p2_inc
         p3_delta
-
         Returns
         -------
         ls_resultado
-
         Debugging
         ---------
         """
@@ -94,7 +91,6 @@ def f_precios_masivos(p0_fini, p1_ffin, p2_gran, p3_inst, p4_oatk, p5_ginc):
         # Para debuging
         # print(f1 + ' y ' + f2)
         lista = list()
-
         # Acomodar las llaves
         for i in range(len(a1_hist['candles']) - 1):
             lista.append({'TimeStamp': a1_hist['candles'][i]['time'],
@@ -169,3 +165,241 @@ def f_precios_masivos(p0_fini, p1_ffin, p2_gran, p3_inst, p4_oatk, p5_ginc):
         r_df_final['Close'] = pd.to_numeric(r_df_final['Close'], errors='coerce')
 
         return r_df_final
+    
+############################# funciones nuevas 
+
+def f_leer_archivo(param_archivo):
+    
+    """
+    Parameters
+    ----------
+    param_archivo : xlsx that will be read.
+    
+    ---------
+    description
+    put the location of the xlsx 
+    get a data frame from the selected xlsx 
+    """
+    archivo=pd.read_excel(param_archivo,skiprows=4)
+    archivo=archivo.iloc[:18]
+    
+    #fill with 0
+    archivo['Close Time']=archivo['Close Time'].fillna(0)
+    
+    #eliminar los balance para mantener solo buy y sell
+    archivo=archivo[archivo.Type != 'balance']
+    return archivo
+
+def f_pip_size(param_ins):
+    """
+    Parameters
+    ----------
+    param_ins : string with the name of the pair currency you are trading
+    
+    Returns
+    -------
+    pip_inst : func :number of pipss of the currency. 
+    
+    Debugging
+    ---------
+    """
+
+    #transfom to lower
+    inst = param_ins.lower()
+    # dictionary with pips per trading currency
+    pip_inst = {'usdjpy': 100, 'gbpjpy': 100, 'eurjpy': 100, 'cadjpy': 100,
+                'chfjpy': 100,
+                'eurusd': 10000, 'gbpusd': 10000, 'usdcad': 10000, 'usdmxn': 10000,
+                'audusd': 10000, 'nzdusd': 10000, 'usdchf': 10000, 'eurgbp': 10000,
+                'eurchf': 10000, 'eurnzd': 10000, 'euraud': 10000, 'gbpnzd': 10000,
+                'gbpchf': 10000, 'gbpaud': 10000, 'audnzd': 10000, 'nzdcad': 10000,
+                'audcad': 10000, 'xauusd': 10, 'xagusd': 10, 'btcusd': 1}
+
+    return pip_inst[inst]
+
+
+#%%
+def f_columnas_tiempos(param_data):
+    """
+    Parameters
+    ----------
+    param_data : pd.DataFrame : df con información de transacciones ejecutadas
+    
+    Returns
+    -------
+    param_data : pd.DataFrame : df con columna agregada 'time'
+    
+    Debugging
+    ---------
+    param_data = f_leer_archivo("trade3.xlsx)
+    """
+    #here i define the index of the clean data 
+    a=[1,2,3,4,6,7,8,11,13,14,17]
+
+    # convert into datetie 
+    param_data['Close Time'] = pd.to_datetime(param_data['Close Time'])
+    param_data['Open Time'] = pd.to_datetime(param_data['Open Time'])
+    
+   #create time column
+    param_data['tiempo'] = [(param_data.loc[i, 'Close Time'] -param_data.loc[i, 'Open Time']).delta / 1e9 for i in a]
+        #param_data['tiempo']=
+        #param_data['tiempo'][i]=param_data['Close Time'][i]-param_data['Open Time'][i]
+    # return param_data['time']
+    return param_data
+
+#%%
+# Nuevas columnas pips para saber el tamaño en pips de las transacciones ejecutadas 
+
+def f_columnas_pips(data):
+    """
+    Parameters
+    ----------
+    datos : pd.DataFrame : dataframe con las transacciones ejecutadas ya con la columna 'tiempos'
+    Returns
+    -------
+    param_data : pd.DataFrame : dataframe anterior pero con columnas 'pips' y 'pips acumulados'
+    
+    Debugging
+    ---------
+    datos =  f_leer_archivo("trade3.xlsx")
+    
+    """
+
+    
+    a=[1,2,3,4,6,7,8,11,13,14,17]
+    for i in a:
+        currency=data['Item'][i]
+        data['Item'][i]=currency.split('-')[0]
+    
+    data['pips'] = [(data['Price.1'][i] - data['Price'][i])*f_pip_size(data['Item'][i]) for i in a]
+    data['pips'][data['Type'] == 'sell'] *= -1
+    data['pips_acm'] = data.pips.cumsum()
+    data['profit_acm'] = data['Profit'].cumsum()
+    data['capital_acm'] = 5000 + data.profit_acm 
+    
+    return data
+
+#%%  Estadísticas básicas
+
+def f_estadisticas_ba(datos):
+    """
+    Parameters
+    ----------
+      datos : pd.DataFrame : DataFrame con transacciones después de correr las funciones anteriores
+    
+    Returns
+    -------
+    Dos dataframes:
+    df_1_tabla : pd.DataFrame :  DataFrame  el cual muestre el ratio de efectividad de las operaciones realizadas con cada instrumento operado
+    df_2_ranking : pd.DataFrame : dataframe con un ranking entre el 0 y el 1 en donde se califica con cuales divisas se obtuvieron operaciones precisas realizadas
+        
+    Debugging
+    ---------
+    datos = f_leer_archivo("trade3.xlsx")
+    """
+    
+    df_1_tabla = pd.DataFrame({'Ops totales': [len(datos), 'Operaciones totales'],
+                                'Ops ganadoras': [len(datos[datos['Profit'] >= 0]), 'Operaciones ganadoras'],
+                                'Ops ganadoras_b': [len(datos[(datos['Type'] == 'buy') & (datos['Profit'] >= 0)]), 'Operaciones ganadoras en compra'],
+                                'Ops ganadoras_s': [len(datos[(datos['Type'] == 'sell') & (datos['Profit'] >= 0)]), 'Operaciones ganadoras en venta'],
+                                'Ops perdedoras': [len(datos[datos['Profit'] < 0]), 'Operaciones perdedoras'],
+                                'Ops perdedoras_b': [len(datos[(datos['Type'] == 'buy') & (datos['Profit'] < 0)]), 'Operaciones perdedoras en compra'],
+                                'Ops perdedoras_s' : [len(datos[(datos['Type'] == 'sell') & (datos['Profit'] < 0)]), 'Operaciones perdedoras en venta'],
+                                'Profit media': [datos['Profit'].median(), 'Mediana de profit de operaciones'],
+                                'Pips media': [datos['pips'].median(), 'Mediana de pips de operaciones'],
+                                'r_efectividad': [len(datos[datos['Profit'] >= 0])/len(datos), 'Ganadoras Totales/Operaciones Totales'],
+                                'r_proporcion': [len(datos[datos['Profit'] >= 0]) / len(datos[datos['Profit'] < 0]), 'Perdedoras Totales/Ganadoras Totales'],
+                                'r_efectividad_b': [len(datos[(datos['Type'] == 'buy') & (datos['Profit'] >= 0)]) / len(datos), 'Operaciones ganadoras de compra/Operaciones Totales'],
+                                'r_efectividad_s': [len(datos[(datos['Type'] == 'sell') & (datos['Profit'] >= 0)]) / len(datos), 'Operaciones ganadoras de venta/Operaciones Totales'],
+                                }, index = ['Valor', 'Descripción']).transpose()
+    
+    tb1 = pd.DataFrame({i: len(datos[datos.Profit >0][datos.Item == i])/len(datos[datos.Item == i])
+                        for i in datos.Item.unique()}, index = ['rank']).transpose()
+    
+    df_2_ranking = (tb1*100).sort_values(by = 'rank', ascending = False).T.transpose()
+    
+    return {'df_1_tabla' : df_1_tabla.copy(), 'df_2_ranking' : df_2_ranking.copy()}
+
+#%% Parte 3
+def f_profit_diario(datos):
+     """
+     Parameters
+     ----------
+     datos : pandas.DataFrame : dataframe de fechas históricas solo usando columnas timestamp y profit
+  
+     Returns
+     -------
+     datos : pandas.DataFrame : dataframe con las columnas timestamp, profit diario y el acumulado
+  
+     Debugging
+     ---------
+     datos = f_leer_archivo("trade3.xlsx")
+     """
+     # cantidad de operaciones cerradas ese dia
+     datos['ops'] = [i.date() for i in datos.closetime] 
+     diario = pd.date_range(datos.ops.min(),datos.ops.max()).date
+     # convertir a dataframe las fechas diarias
+     fechas = pd.DataFrame({'timestamp' : diario})
+     
+     groups = datos.groupby('ops')
+     profit = groups['profit'].sum()
+     # convertir los profits diarios a dataframe
+     profit_diario = pd.DataFrame({'profit_d' : [profit[i] if i in profit.index else 0 for i in diario]})
+     profit_acm = np.cumsum(profit_diario) + 5000
+     # juntar en un solo dataframe los dos dataframes anteriores fechas y profits diarios
+     f_p1 =pd.merge(fechas, profit_diario, left_index = True, right_index = True)
+     # juntar el dataframe anterior de los dos df con los profits acumulados
+     df_profit_diario1 = pd.merge(f_p1, profit_acm, left_index = True, right_index = True)
+     # renombrar las columnas del nuevo dataframe
+     df_profit_diario = df_profit_diario1.rename(columns = {"profit_d_x" : "profit_d", "profit_d_y" : "profit_acm_d"})
+     
+     return df_profit_diario
+  
+
+#%%
+# Terminar el profit acumulado diario antes de terminar stats mad
+def f_estadisticas_mad(datos):
+    """
+    Parameters
+    ----------
+    datos : DataFrame con transacciones actualizadas
+    
+    Returns
+    -------
+    datos : pandas.DataFrame : dataframe con rendimientos logarítmicos. Tomando en cuenta que se inicializa con una cuenta de $5,000
+    Debugging
+    ---------
+    datos = 'f_leer_archivo("archivo_tradeview_1.csv")
+    
+    """
+    rend_log = np.log(datos.capital_acm[1:].values/datos.capital_acm[:-1].values)
+    # benchmark = 
+    # rend_log_bench = np.log(datos_benchmark...)
+    # tracking_error = rend_log - rend_log_bench
+    
+    # https://towardsdatascience.com/python-for-finance-stock-portfolio-analyses-6da4c3e61054
+    # https://www.investopedia.com/terms/i/informationratio.asp    
+    rf = 0.08
+    
+    #%%
+    #TIP DEL PROFE
+    #df_data.groupby('fechas_dias')['profit'].sum()
+    #%%
+    # Cambiar a semanal los datos
+    # Agregar un benchmark SP500
+    MAD = pd.DataFrame({
+        'sharpe': (rend_log.mean()*30 - rf) / rend_log.std()*(30**0.5),
+        'sortino_b': (rend_log.mean()*30 - rf) / rend_log[rend_log >= 0].std()*(30**0.5),
+        'sortino_s': (rend_log.mean()*30 - rf) / rend_log[rend_log < 0].std()*(30**0.5),
+        # Que de donde se inicia, no hayan valores mayores al punto de inicio y sea solo tendencia bajista
+        # 'drawdown_cap': 
+        # Que de donde se inicia, no hayan valores menores al punto de inicio y que sea solo tendencia alcista
+        # 'drawup_cap': 
+        #'drawdown_pips': datos.pips_acm...,
+        #'drawup_pips': datos.pips_acm.. #,
+        # 'information_ratio': (rend_log.mean()*7 - rend_log_bench.mean()*7) / tracking_error.std()*7**0.5
+        }, index = ['Valor']).transpose()
+
+    return MAD
+
+#%%  Parte 4 
